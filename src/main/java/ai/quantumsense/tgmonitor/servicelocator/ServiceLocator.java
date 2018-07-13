95fb29f029cec26440585704b6c6c060727af6f3
Daniel Weibel <danielmweibel@gmail.com>
2018-07-13 15:37:20 +0800

    Change to pure interface component

--- src/main/java/ai/quantumsense/tgmonitor/servicelocator/ServiceLocator.java
+++ src/main/java/ai/quantumsense/tgmonitor/servicelocator/ServiceLocator.java
@@ -1,17 +1,6 @@
 package ai.quantumsense.tgmonitor.servicelocator;
 
-import ai.quantumsense.tgmonitor.backend.Interactor;
-import ai.quantumsense.tgmonitor.monitor.LoginCodePrompt;
-import ai.quantumsense.tgmonitor.monitor.entities.Emails;
-import ai.quantumsense.tgmonitor.monitor.entities.Patterns;
-
-public interface ServiceLocator {
-    void registerPatternsRepo(Patterns patterns);
-    Patterns getPatternsRepo();
-    void registerEmailsRepo(Emails emails);
-    Emails getEmailsRepo();
-    void registerInteractor(Interactor interactor);
-    Interactor getInteractor();
-    void registerLoginCodePrompt(LoginCodePrompt loginCodePrompt);
-    LoginCodePrompt getLoginCodePrompt();
+public interface ServiceLocator<T> {
+    void registerService(T type);
+    T getService();
 }
